5697c629a8d8846801ff82b929a17dc572ee345b
Philip Musonye <141995654+PMUM@users.noreply.github.com>
2026-09-22 20:33:58 +0300

    Complete exercise 1: variables, console.log, if/else

--- week-01/exercise1.js
+++ week-01/exercise1.js
@@ -4,23 +4,31 @@
 // Fill in each TODO below, then run:  node week-01/exercise1.js
 
 // TODO 1: create a variable called `name` holding your name (a string)
+let name = "Philip";
 
 
 // TODO 2: create a variable called `age` holding your age (a number)
-
+let age = 30;
 
 // TODO 3: create a variable called `isLearningToCode` set to true (a boolean)
-
+let isLearningToCode = true;
 
 // TODO 4: print a sentence using your variables, e.g.:
 //   console.log(name + " is " + age + " years old.");
-
+console.log(name + " is " + age + " years old ");
 
 // TODO 5: write an if/else that checks `age`:
-//   if age is 18 or older, print "You are an adult."
-//   otherwise print "You are a minor."
-
+if (age >= 18) {
+    console.log("You are an adult.");
+} else {
+    console.log("You are a minor.");
+}
 
 // TODO 6: BONUS — write an if/else that checks `isLearningToCode`:
 //   if true, print "Currently learning to code!"
 //   if false, print "Not learning right now."
+if (isLearningToCode) {
+    console.log("Currently learning to code!");
+} else {
+    console.log("Not learning right now.");
+}
